b39b731a3e4cb3fa6d1012fbbbcd2470c908e7e0
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-07-15 17:29:57 +0200

    Fix install prefix

--- install_tl.py
+++ install_tl.py
@@ -34,3 +34,7 @@ if __name__ == '__main__':
             print(child.readline().decode().strip())
     except pexpect.EOF:
         print('Installation seems to be finished')
+
+    if 'Welcome' not in child.read():
+        print('Installation did not terminate successfull')
+        sys.exit(1)
