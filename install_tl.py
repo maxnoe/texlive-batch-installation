34b3e58211afb523ee6007385e5e12ccf7a84476
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-07-15 13:01:27 +0200

    Add tex file, install script and .travis.yaml

---
+++ install_tl.py
@@ -0,0 +1,32 @@
+import pexpect
+from glob import glob
+import os
+
+
+if __name__ == '__main__':
+    install_script = glob('install-tl-*/install-tl')
+
+    os.environ['TEXLIVE_INSTALL_PREFIX'] = '/home/travis/texlive'
+    child = pexpect.spawn(install_script)
+
+    try:
+        child.expect('Import settings from previous TeX Live installation', timeout=10)
+        print(child.before.decode())
+        print(child.after.decode())
+        child.sendline('n')
+    except pexpect.TIMEOUT:
+        print('No previous installation found')
+
+    try:
+        child.expect('Enter command:', timeout=10)
+        print(child.before.decode())
+        print(child.after.decode())
+        child.sendline('I')
+    except pexpect.TIMEOUT:
+        print('Something went wrong')
+
+    try:
+        while True:
+            print(child.readline().decode().strip())
+    except pexpect.EOF:
+        print('Installation seems to be finished')
