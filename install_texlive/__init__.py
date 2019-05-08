b53510384690ed47481b02e6f9a1aa0b09fa8801
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-05-08 11:48:13 +0200

    More fixes

--- install_texlive/__init__.py
+++ install_texlive/__init__.py
@@ -3,7 +3,8 @@ import subprocess as sp
 import os
 import pexpect
 import re
-from datetime import date
+import requests
+import time
 
 log = logging.getLogger(__name__)
 
@@ -15,10 +16,14 @@ OLDURL = 'ftp://tug.org/historic/systems/texlive/{v}/tlnet-final/'
 
 
 def is_current(version):
-    today = date.today()
-    current_year = int(version) == today.year
-    last_year = int(version) == (today.year - 1) and today.month < 7
-    return current_year or last_year
+    r = requests.get('https://tug.org/texlive/')
+    r.raise_for_status()
+
+    m = re.search(r'Current release: TeX Live ([0-9]{4})', r.text)
+    if not m:
+        raise ValueError('Could not determine current TeX Live version')
+
+    return int(m.groups()[0]) == version
 
 
 def download(version=None, outdir='.'):
@@ -31,12 +36,20 @@ def download(version=None, outdir='.'):
 
     if has_curl:
         log.info('Start downloading TeX Live {} using curl'.format(version or 'current'))
-        download = sp.Popen(['curl', '--fail', '-L', url], stdout=sp.PIPE, stderr=sp.PIPE)
+        download = sp.Popen(['curl', '--fail', '-sSL', url], stdout=sp.PIPE, stderr=sp.PIPE)
     elif has_wget:
         log.info('Start downloading TeX Live {} using wget'.format(version or 'current'))
         download = sp.Popen(['wget', '-qO-', url], stdout=sp.PIPE, stderr=sp.PIPE)
     else:
         raise IOError('Either curl or wget required')
+
+    # check if download could be started
+    time.sleep(0.5)
+    code = download.poll()
+    if code is not None and code != 0:
+        raise ConnectionError('Could not download texlive: {}'.format(
+            download.stderr.read().decode()
+        ))
 
     sp.check_call(['tar', 'xz', '--directory', outdir], stdin=download.stdout)
 
--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     ],
     install_requires=[
         'pexpect',
+        'requests',
     ],
     entry_points={
         'console_scripts': [
